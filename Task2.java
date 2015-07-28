d3ea0a9cba11a045dd4a464e25fd3310c8abe5ed
Kyrylo <zakharovkyrylo@gmail.com>
2015-07-28 11:02:19 +0300

    home03

--- Task2.java
+++ Task2.java
@@ -5,30 +5,29 @@ import java.util.Scanner;
 
 public class Task2 {
 
-    void game(int random){
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("enter number");
-        int num=scanner.nextInt();
-        if(num==random) {
-            System.out.println("congratulations");
-            return;
-        }
-        else if(num>random){
-            System.out.println("too high");
-            game(random);
-        }
-        else {
-            System.out.println("too low");
-            game(random);
-        }
-    }
-
     public static void main(String[] args) {
-        Task2 task2=new Task2();
+        Game game = new Game();
         Random r= new Random();
         int random=r.nextInt(99+1);
 //        System.out.println(random);
-        task2.game(random);
+        game.guessTheNumber(random);
+    }
+}
 
+class Game{
+    void guessTheNumber(int random){
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter number");
+            int num = scanner.nextInt();
+            if (num == random) {
+                System.out.println("congratulations");
+                return;
+            } else if (num > random) {
+                System.out.println("too high");
+            } else {
+                System.out.println("too low");
+            }
+        }
     }
 }
